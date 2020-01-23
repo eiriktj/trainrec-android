f2811e9c0152bf45d95be5222c526bda05a11545
Eirik Jensen <eiriktj@pm.me>
2020-01-23 07:04:38 +0100

    Rename App activity MainActivity. Change RelativeLayout to LinearLayout

--- app/src/main/java/no/trainrec/android/MainActivity.kt
+++ app/src/main/java/no/trainrec/android/MainActivity.kt
@@ -3,7 +3,7 @@ package no.trainrec.android;
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class App : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
