4bdb8f697417b2af38c98d3fbb2d512c7881ddc8
Eirik Jensen <eiriktj@pm.me>
2020-06-21 17:47:22 +0200

    Cleanup MainActivity.

--- app/src/main/java/no/trainrec/android/MainActivity.kt
+++ app/src/main/java/no/trainrec/android/MainActivity.kt
@@ -3,22 +3,13 @@ package no.trainrec.android;
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
-import androidx.compose.MutableState
 import androidx.compose.state
 import androidx.ui.core.Alignment
 import androidx.ui.core.Modifier
 import androidx.ui.core.setContent
 import androidx.ui.foundation.Text
-import androidx.ui.graphics.Color
 import androidx.ui.layout.Column
-import androidx.ui.layout.fillMaxHeight
-import androidx.ui.layout.padding
-import androidx.ui.material.Button
-import androidx.ui.material.Divider
-import androidx.ui.material.MaterialTheme
 import androidx.ui.material.Surface
-import androidx.ui.tooling.preview.Preview
-import androidx.ui.unit.dp
 import androidx.ui.material.Tab
 import androidx.ui.material.TabRow
 
@@ -42,7 +33,7 @@ fun MyApp() {
 
 @Composable
 fun MyContent() {
-    val clickedState: MutableState<Int> = state { 0 }
+    val clickedState = state { 0 }
     val titles = listOf("TAB 1", "TAB 2")
 
     Column {
@@ -59,12 +50,6 @@ fun MyContent() {
         Text(
         modifier = Modifier.gravity(Alignment.CenterHorizontally),
         text = "Text tab ${clickedState.value + 1} selected",
-        style = MaterialTheme.typography.body1
         )
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text (text = "Hello $name!")
 }
