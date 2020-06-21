903ecb1d653c9445a4d44c6402229c397be048ee
Eirik Jensen <eiriktj@pm.me>
2020-06-21 17:25:26 +0200

    Add tabrow with 2 tabs

--- app/src/main/java/no/trainrec/android/MainActivity.kt
+++ app/src/main/java/no/trainrec/android/MainActivity.kt
@@ -3,30 +3,68 @@ package no.trainrec.android;
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.MutableState
+import androidx.compose.state
+import androidx.ui.core.Alignment
+import androidx.ui.core.Modifier
 import androidx.ui.core.setContent
-import androidx.ui.core.TextField
+import androidx.ui.foundation.Text
+import androidx.ui.graphics.Color
 import androidx.ui.layout.Column
+import androidx.ui.layout.fillMaxHeight
+import androidx.ui.layout.padding
 import androidx.ui.material.Button
-import androidx.ui.material.surface.Surface
+import androidx.ui.material.Divider
+import androidx.ui.material.MaterialTheme
+import androidx.ui.material.Surface
+import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
+import androidx.ui.material.Tab
+import androidx.ui.material.TabRow
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppTheme {
-                Surface {
-                    App()
-                }
-            }
+                    MyApp()
         }
     }
 }
 
 @Composable
-fun App() {
-    Column {
-        TextField(value = "")
-        Button(text = "")
-        Button(text = "")
+fun MyApp() {
+    MyAppTheme {
+        Surface {
+            MyContent()
+        }
     }
+}
+
+@Composable
+fun MyContent() {
+    val clickedState: MutableState<Int> = state { 0 }
+    val titles = listOf("TAB 1", "TAB 2")
+
+    Column {
+        TabRow(
+            items = titles,
+            selectedIndex = clickedState.value,
+        ) { index, text ->
+            Tab(
+                text = { Text(text) },
+                selected = clickedState.value == index,
+                onSelected = { clickedState.value = index }
+            )
+        }
+        Text(
+        modifier = Modifier.gravity(Alignment.CenterHorizontally),
+        text = "Text tab ${clickedState.value + 1} selected",
+        style = MaterialTheme.typography.body1
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text (text = "Hello $name!")
 }
