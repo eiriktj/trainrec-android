e79f09b5122dcf2cb09119e91f11f33906697213
Eirik Jensen <eiriktj@pm.me>
2020-06-21 17:29:30 +0200

    Change app theme to material darkcolorpalette

--- app/src/main/java/no/trainrec/android/AppTheme.kt
+++ app/src/main/java/no/trainrec/android/AppTheme.kt
@@ -1,25 +1,12 @@
 package no.trainrec.android;
 
 import androidx.compose.Composable
-import androidx.ui.graphics.Color
-import androidx.ui.material.ColorPalette
 import androidx.ui.material.MaterialTheme
-
-private val darkThemeColors = ColorPalette(
-    primary = Color(0xFFEA6D7E),
-    primaryVariant = Color(0xFFDD0D3E),
-    onPrimary = Color.Black,
-    secondary = Color(0xFF121212),
-    onSecondary = Color.White,
-    surface = Color(0xFF121212),
-    background = Color(0xFF121212),
-    onBackground = Color.White,
-    onSurface = Color.White
-)
+import androidx.ui.material.darkColorPalette
 
 @Composable
-fun AppTheme(children: @Composable() () -> Unit) {
-    MaterialTheme(colors = darkThemeColors) {
-        children()
+fun MyAppTheme(content: @Composable() () -> Unit) {
+    MaterialTheme(colors = darkColorPalette()) {
+        content()
     }
 }
