1701be87f0365191e5b49fff518aa3a017f95ca9
Eirik Jensen <eiriktj@pm.me>
2020-06-24 13:55:09 +0200

    Take input from textfield

--- app/src/main/java/no/trainrec/android/MainActivity.kt
+++ app/src/main/java/no/trainrec/android/MainActivity.kt
@@ -1,6 +1,7 @@
 package no.trainrec.android;
 
 import android.os.Bundle
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.compose.state
@@ -8,10 +9,18 @@ import androidx.ui.core.Alignment
 import androidx.ui.core.Modifier
 import androidx.ui.core.setContent
 import androidx.ui.foundation.Text
+import androidx.ui.foundation.TextField
+import androidx.ui.foundation.TextFieldValue
+import androidx.ui.graphics.Color
+import androidx.ui.input.ImeAction
+import androidx.ui.input.KeyboardType
 import androidx.ui.layout.Column
+import androidx.ui.layout.fillMaxWidth
+import androidx.ui.layout.padding
 import androidx.ui.material.Surface
 import androidx.ui.material.Tab
 import androidx.ui.material.TabRow
+import androidx.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +43,7 @@ fun MyApp() {
 @Composable
 fun MyContent() {
     val clickedState = state { 0 }
-    val titles = listOf("TAB 1", "TAB 2")
+    val titles = listOf("Add", "List")
 
     Column {
         TabRow(
@@ -47,9 +56,37 @@ fun MyContent() {
                 onSelected = { clickedState.value = index }
             )
         }
-        Text(
-        modifier = Modifier.gravity(Alignment.CenterHorizontally),
-        text = "Text tab ${clickedState.value + 1} selected",
-        )
+        //Text(
+        //modifier = Modifier.gravity(Alignment.CenterHorizontally),
+        //text = "Text tab ${clickedState.value + 1} selected",
+        //)
+        when(clickedState.value) {
+            0 -> AddTab()
+            1 -> ListTab()
+        }
     }
 }
+
+@Composable
+fun AddTab() {
+    val textFieldState = state { TextFieldValue("") }
+    val textOutput = state { "" }
+    Surface(color = Color.DarkGray, modifier = Modifier.padding(16.dp)) {
+        TextField(
+            value = textFieldState.value,
+            keyboardType = KeyboardType.Text,
+            modifier = Modifier.padding(16.dp) + Modifier.fillMaxWidth(),
+            onValueChange = { textFieldState.value = it },
+            onImeActionPerformed = { 
+                textOutput.value = textFieldState.value.text
+            }
+        )
+    }
+    Text(textOutput.value)
+}
+
+@Composable
+fun ListTab() {
+    Text( text = "ListTab")
+}
+
